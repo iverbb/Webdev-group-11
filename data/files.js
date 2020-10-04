62701328528fe2dd0d469de7edcc94821d82d0bf
iverbb <iverbb@stud.ntnu.no>
2020-10-04 16:19:06 +0800

    removed duplications, fixed filelist

--- data/files.js
+++ data/files.js
@@ -1,209 +1,192 @@
-data = { "folder" :
-         { "name" : "Project",
-           "description" : "Project files",
-           "files"  :
-           [ { "folder" :
-               { "name" : "html/",
-                 "description" : "HTML webpages and templates",
-                 "files" :
-                 [ { "file" :
-                     { "name" : "hjem.html",
-                       "description" : ""}},
-                   { "file" :
-                     { "name" : "enkeltprosjekt.html",
-                       "description" : ""}},
-                   { "file" :
-                     { "name" : "prosjekt_prosjekttype.html",
-                       "description" : ""}},
-                   { "file" :
-                     { "name" : "prosjekt_kronologisk.html",
-                       "description" : ""}},
-                   { "file" :
-                     { "name" : "prosjekt_ideer.html",
-                       "description" : ""}},
-                   { "file" :
-                     { "name" : "prosjekt_beliggenhet.html",
-                       "description" : ""}},
-                   { "file" :
-                     { "name" : "paa_tegnebordet.html",
-                       "description" : ""}},
-                     { "name" : "ideer.html",
-                       "description" : ""}},
-                   { "file" :
-                     { "name" : "om_oss.html",
-                       "description" : ""}},
-                   { "file" :
-                     { "name" : "enkeltprosjekt.html",
-                       "description" : ""}},
-                   { "file" :
-                     { "name" : "kontakt.html",
-                       "description" : ""}}
-                 ]
-               }
-             },
-             { "folder" :
-               { "name" : "css/",
-                 "description" : "Stylesheets",
-                 "files" :
-                 [ { "file" :
-                     { "name" : "main.css",
-                       "description" : ""
-                     }
-                   },
-                   { "file" :
-                       { "name" : "hjem.css",
-                         "description" : ""
-                       }
-                   },
-                   { "file" :
-                     { "name" : "prosjekter.css",
-                       "description" : ""
-                     }
-                   },
-                   { "file" :
-                     { "name" : "paa_tegnebordet.css",
-                       "description" : ""
-                     }
-                   },
-                   { "file" :
-                     { "name" : "om_oss.css",
-                       "description" : ""
-                     }
-                   },
-                   { "file" :
-                     { "name" : "ideer.css",
-                       "description" : ""
-                     }
-                   },
-                   { "file" :
-                     { "name" : "beliggenhet.css",
-                       "description" : ""
-                     }
-                   },
-                   { "file" :
-                     { "name" : "enkeltprosjekt.css",
-                       "description" : ""
-                     }
-                   },
-                   { "file" :
-                     { "name" : "kontakt.css",
-                       "description" : ""
-                     }
-                   },
-                   { "file" :
-                     { "name" : "galleri.css",
-                       "description" : ""
-                     }
-                   },
-                   { "file" :
-                     { "name" : "kart.css",
-                       "description" : ""
-                     }
-                   },
-                   { "file" :
-                     { "name" : "enkeltprosjekt.css",
-                       "description" : ""
-                     }
-                   }
-                 ]
-               }
-             },
-             { "folder" :
-               { "name" : "img/",
-                 "description" : "images of various kinds",
-                 "files" :
-                 [ { "file" :
-                     { "name" : "prosjekt1.png",
-                       "description" : ""}},
-                   { "file" :
-                     { "name" : "project2_1.png",
-                       "description" : ""}},
-                   { "file" :
-                     { "name" : "project2_2.png",
-                       "description" : ""}},
-                   { "file" :
-                     { "name" : "projectn.png",
-                       "description" : ""}},
-                   { "file" :
-                     { "name" : "logo.png",
-                       "description" : ""}},
-                   { "file" :
-                     { "name" : "forside.png",
-                       "description" : ""}},
-                   { "file" :
-                     { "name" : "markoer.png",
-                       "description" : ""}},
-                   { "file" :
-                     { "name" : "kart.png",
-                       "description" : ""}},
-                   { "file" :
-                     { "name" : "ansattx.png",
-                       "description" : ""}},
-                   { "file" :
-                     { "name" : "readme.md",
-                       "description" : ""}},
-                 ]
-               }
-             },
-             { "folder" :
-               { "name" : "script/",
-                 "description" : "Scripts for interactive content",
-                 "files" :
-                 [ { "file" :
-                     { "name" : "kart.js",
-                       "description" : ""}},
-                   { "file" :
-                     { "name" : "enkeltprosjekt.js",
-                       "description" : ""}},
-                   { "file" :
-                     { "name" : "marg.js",
-                       "description" : ""}},
-                   { "file" :
-                     { "name" : "tidslinje.js",
-                       "description" : ""}},
-                   { "file" :
-                     { "name" : "enkeltprosjekt.js",
-                       "description" : ""}},
-                   { "file" :
-                     { "name" : "bildeviser.js",
-                       "description" : ""}},
-                   { "file" :
-                     { "name" : "galleri.js",
-                       "description" : ""}},
-                   { "file" :
-                     { "name" : "gallerimeny.js",
-                       "description" : ""}},
-                   { "file" :
-                     { "name" : "marg.js",
-                       "description" : ""}},
-                   { "file" :
-                     { "name" : "laster.js",
-                       "description" : ""}},
-                   { "file" :
-                     { "name" : "galleri.js",
-                       "description" : ""}},
-                   { "file" :
-                     { "name" : "gallerimeny.js",
-                       "description" : ""}}
-                 ]
-               }
-             },
-             { "folder" :
-               { "name" : "data/",
-                 "description" : "Files containing various data related to the page",
-                 "files" :
-                 [ { "file" :
-                     { "name" : "data.js",
-                       "description" : ""}},
-                   { "file" :
-                     { "name" : "readme.md",
-                       "description" : ""}}
-                 ]
-               }
-             },
-             { "file" :
-               { "name" : "readme.md",
-                 "description" : ""}}
-           ]
-         }
-       };
+data =
+    { "folder" :
+      { "name" : "Project",
+        "description" : "Project files",
+        "files"  :
+        [ { "folder" :
+            { "name" : "html/",
+              "description" : "HTML webpages and templates",
+              "files" :
+              [ { "file" :
+                  { "name" : "hjem.html",
+                    "description" : ""}},
+                { "file" :
+                  { "name" : "enkeltprosjekt.html",
+                    "description" : ""}},
+                { "file" :
+                  { "name" : "prosjekt_prosjekttype.html",
+                    "description" : ""}},
+                { "file" :
+                  { "name" : "prosjekt_kronologisk.html",
+                    "description" : ""}},
+                { "file" :
+                  { "name" : "prosjekt_ideer.html",
+                    "description" : ""}},
+                { "file" :
+                  { "name" : "prosjekt_beliggenhet.html",
+                    "description" : ""}},
+                { "file" :
+                  { "name" : "paa_tegnebordet.html",
+                    "description" : ""}},
+                { "file" :
+                  { "name" : "ideer.html",
+                    "description" : ""}},
+                { "file" :
+                  { "name" : "om_oss.html",
+                    "description" : ""}},
+                { "file" :
+                  { "name" : "enkeltprosjekt.html",
+                    "description" : ""}},
+                { "file" :
+                  { "name" : "kontakt.html",
+                    "description" : ""}},
+              ]
+            }
+          },
+          { "folder" :
+            { "name" : "css/",
+          "description" : "Stylesheets",
+          "files" :
+          [ { "file" :
+              { "name" : "main.css",
+                "description" : ""
+              }
+            },
+            { "file" :
+              { "name" : "hjem.css",
+                "description" : ""
+              }
+            },
+            { "file" :
+              { "name" : "prosjekter.css",
+                "description" : ""
+              }
+            },
+            { "file" :
+              { "name" : "paa_tegnebordet.css",
+                "description" : ""
+              }
+            },
+            { "file" :
+              { "name" : "om_oss.css",
+                "description" : ""
+              }
+            },
+            { "file" :
+              { "name" : "ideer.css",
+                "description" : ""
+              }
+            },
+            { "file" :
+              { "name" : "beliggenhet.css",
+                "description" : ""
+              }
+            },
+            { "file" :
+              { "name" : "enkeltprosjekt.css",
+                "description" : ""
+              }
+            },
+            { "file" :
+              { "name" : "kontakt.css",
+                "description" : ""
+              }
+            },
+            { "file" :
+              { "name" : "galleri.css",
+                "description" : ""
+              }
+            },
+          ]
+        }
+      },
+      { "folder" :
+        { "name" : "img/",
+          "description" : "images of various kinds",
+          "files" :
+          [ { "file" :
+              { "name" : "prosjekt1.png",
+                "description" : ""}},
+            { "file" :
+              { "name" : "project2_1.png",
+                "description" : ""}},
+            { "file" :
+              { "name" : "project2_2.png",
+                "description" : ""}},
+            { "file" :
+              { "name" : "projectn.png",
+                "description" : ""}},
+            { "file" :
+              { "name" : "logo.png",
+                "description" : ""}},
+            { "file" :
+              { "name" : "forside.png",
+                "description" : ""}},
+            { "file" :
+              { "name" : "markoer.png",
+                "description" : ""}},
+            { "file" :
+              { "name" : "kart.png",
+                "description" : ""}},
+            { "file" :
+              { "name" : "ansattx.png",
+                "description" : ""}},
+            { "file" :
+              { "name" : "readme.md",
+                "description" : ""}},
+          ]
+        }
+      },
+      { "folder" :
+        { "name" : "script/",
+          "description" : "Scripts for interactive content",
+          "files" :
+          [ { "file" :
+              { "name" : "kart.js",
+                "description" : ""}},
+            { "file" :
+              { "name" : "marg.js",
+                "description" : ""}},
+            { "file" :
+              { "name" : "tidslinje.js",
+                "description" : ""}},
+            { "file" :
+              { "name" : "enkeltprosjekt.js",
+                "description" : ""}},
+            { "file" :
+              { "name" : "bildeviser.js",
+                "description" : ""}},
+            { "file" :
+              { "name" : "galleri.js",
+                "description" : ""}},
+            { "file" :
+              { "name" : "gallerimeny.js",
+                "description" : ""}},
+            { "file" :
+              { "name" : "marg.js",
+                "description" : ""}},
+            { "file" :
+              { "name" : "laster.js",
+                "description" : ""}},
+          ]
+        }
+      },
+      { "folder" :
+        { "name" : "data/",
+          "description" : "Files containing various data related to the page",
+          "files" :
+          [ { "file" :
+              { "name" : "data.js",
+                "description" : ""}},
+            { "file" :
+              { "name" : "readme.md",
+                "description" : ""}}
+          ]
+        }
+      },
+      { "file" :
+        { "name" : "readme.md",
+          "description" : ""}}
+    ]
+}
+};
