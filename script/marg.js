5db9fc40ffac662afb7e54df3d8658f440f896c8
Vilde-Aurora <70256296+Vilde-Aurora@users.noreply.github.com>
2020-10-15 22:26:33 +0200

    trying a new way to make the navbar

--- script/marg.js
+++ script/marg.js
@@ -1,5 +1,100 @@
 //Vilde
 
+// pages added to navbar as links
+const pages = [
+    {
+      text: "Hjem",
+      endpoint: "hjem.html",
+    },
+    {
+      text: "Prosjekter",
+      endpoint: "prosjekter.html",
+    },
+    {
+      text: "På tegnebordet",
+      endpoint: "paa_tegnebordet.html",
+    },
+    {
+      text: "Om oss",
+      endpoint: "om_oss.html",
+    },
+    {
+      text: "Kontakt",
+      endpoint: "kontakt.html",
+    },
+  ];
+  
+  let currentEndpoint = "";
+  
+  /**
+   * Checks if a given endpoint is the current one
+   * @param {String} endpoint The endpoint to check
+   * @returns {Boolean} true if it is current, else false
+   */
+  function isCurrentEndpoint(endpoint) {
+    return endpoint === currentEndpoint;
+  }
+  
+  /**
+   * Creates an anchor element of a given page with link and content.
+   * @param {Object} page The page with text and endpoint properties
+   * @returns {HTMLAnchorElement} The anchor element
+   */
+  function createLinkToPage(page) {
+    const anchor = document.createElement("a");
+    const text = document.createTextNode(page.text);
+  
+    if (isCurrentEndpoint(page.endpoint)) {
+      anchor.classList.add("navbar-current-item");
+    }
+  
+    anchor.href = page.endpoint;
+    anchor.appendChild(text);
+  
+    return anchor;
+  }
+  
+  /**
+   * Adds a page to the navigation bar as a link.
+   * @param {Object} page The page to link
+   * @param {HTMLDivElement} element The element to hold the link
+   */
+  function addLinkToElement(page, element) {
+    const link = createLinkToPage(page);
+    element.appendChild(link);
+  }
+  
+  /**
+   * Returns the endpoint of the current page
+   * @returns {String} The current endpoint
+   */
+  function getCurrentEndpoint() {
+    const url = window.location.href;
+  
+    return url.split("/").pop();
+  }
+  
+  /**
+   * Builds the navigation bar by adding html elements to it.
+   */
+  function buildNavbar() {
+    currentEndpoint = getCurrentEndpoint();
+  
+    const navbar = document.getElementById("navbar");
+    const content = document.createElement("div");
+    content.classList.add("content");
+    navbar.appendChild(content);
+  
+    const items = document.createElement("div");
+    items.classList.add("navbar-items");
+    content.appendChild(items);
+  
+    pages.forEach((page) => addLinkToElement(page, items));
+  }
+  
+  // Event trigger when the DOM is ready (newer browsers)
+  window.onload = () => buildNavbar();
+
 /* When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
 
