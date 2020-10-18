5bb45be36556a6888e9898efcd4eaaed79e4c6f1
NoraKastellet <nora.kastellet@gmail.com>
2020-10-18 10:21:41 +0200

    test hamburger bar

--- script/hamburger_test.js
+++ script/hamburger_test.js
@@ -1,6 +1,16 @@
+/*
 const hamburger = document.getElementById("hamburger");
 const navUL = document.getElementById("nav-ul");
 
 hamburger.addEventListener("click", () => {
     navUL.classList.toggle("show");
-} );
+} );*/
+
+function myFunction() {
+    var x = document.getElementById("links");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
