f31d8acaf54f91d845ab2b1d8c7294bd4365aeae
Vilde-Aurora <70256296+Vilde-Aurora@users.noreply.github.com>
2020-10-24 10:32:57 +0200

    updating marg

--- script/marg.js
+++ script/marg.js
@@ -2,202 +2,115 @@
 
 //navbar
 
-var img = document.createElement("img");
-img.src = "../p1 and p2/img/P1/logo_rett.png";
-let logo = document.createElement("img");
-
 // pages added to navbar as links
 const pages = [
-    /*{
-      imagePath: "../p1 and p2/img/P1/logo_rett.png", //?? how do i put the picture of the logo here
+  {
+      img: "../p1 and p2/img/P1/logo_rett.png", //?? how do i put the picture of the logo here
       endpoint: "hjem.html",
-    },*/
-    {
-        text: "Hjem",
-        endpoint: "hjem.html",
-    },
-    {
-        text: "Prosjekter",
-        endpoint: "prosjekt_prosjekttype_test.html",
-    },
-    {
-        text: "På tegnebordet",
-        endpoint: "paa_tegnebordet.html",
-    },
-    {
-        text: "Om oss",
-        endpoint: "om_oss.html",
-    },
-    {
-        text: "Kontakt",
-        endpoint: "kontakt.html",
-    },
-  ];
-  
-  let currentEndpoint = "";
-  
-  /**
-   * Checks if a given endpoint is the current one
-   * endpoint: The endpoint to check
-   * returns: true if it is current, else false
-   */
-  function isCurrentEndpoint(endpoint) {
-    return endpoint === currentEndpoint;
-  }
-  
-  /**
-   * Creates an anchor element of a given page with link and content
-   * page: The page with text and endpoint properties
-   * returns anchor element
-   */
-  function createLinkToPage(page) {
-    const anchor = document.createElement("a");
-    /*if (page.text===undefined){
-        const content = document.createElement(page.img); //funker ikke
-    }
-    else {*/
-      const content = document.createTextNode(page.text); //sjekk om tekst, hvis ikke finn img
-    
-  
-  
-    if (isCurrentEndpoint(page.endpoint)) {
-        anchor.classList.add("navbar-current-item");
-    }
+  },
+  {
+      text: "Hjem",
+      endpoint: "hjem.html",
+  },
+  {
+      text: "Prosjekter",
+      endpoint: "prosjekt_prosjekttype_test.html",
+  },
+  {
+      text: "På tegnebordet",
+      endpoint: "paa_tegnebordet.html",
+  },
+  {
+      text: "Om oss",
+      endpoint: "om_oss.html",
+  },
+  {
+      text: "Kontakt",
+      endpoint: "kontakt.html",
+  },
+];
 
-    anchor.href = page.endpoint;
-    anchor.appendChild(content);
-  
-    return anchor;
-  }
-  
-  /**Adds a page to the navigation bar as a link.
-   * page: The page to link
-   * element: The element to hold the link
-   */
-  function addLinkToElement(page, element) {
-    const link = createLinkToPage(page);
-    element.appendChild(link);
-  }
-  
-  //Returns the endpoint of the current page
-  function getCurrentEndpoint() {
-    const url = window.location.href;
-    
-    return url.split("/").pop();
-  }
-  
-  // Builds the navigation bar by adding html elements to it.
-  function buildNavbar() {
-    currentEndpoint = getCurrentEndpoint();
-    
-    const navbar = document.getElementById("navbar");
-    const content = document.createElement("div");
-    content.classList.add("navbar-content");
-    navbar.appendChild(content);
-    
-    const items = document.createElement("div");
-    items.classList.add("navbar-items");
-    content.appendChild(items);
-    
-    pages.forEach((page) => addLinkToElement(page, items));
-  }
+let currentEndpoint = "";
 
-
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-  
- 
-//footer  
-
-function buildFooter() {
-    var node = document.getElementById("myFooter");
-
-    var kontakt = document.createElement("li"); //var breaks = "sdfhdfhg \n efjhsejj \n dhjfjhdsfjk"
-    var gate = document.createElement("li");
-    var postnummer = document.createElement("li");
-    var telefon = document.createElement("li");
-    var epost = document.createElement("li");
-    var copyright = document.createElement("li");
-
-    kontakt.classList.add("tit");
-    copyright.classList.add("copyRight");
-    gate.classList.add("adresse");
-    postnummer.classList.add("adresseSpace");
-    telefon.classList.add("adresse");
-    epost.classList.add("adresse");
-
-    kontakt.appendChild(document.createTextNode("Kontakt oss"));
-    gate.appendChild(document.createTextNode("Avaldsnesgata 95 c"));
-    postnummer.appendChild(document.createTextNode("4014 Stavanger"));
-    telefon.appendChild(document.createTextNode("T:  51 56 78 30"));
-    epost.appendChild(document.createTextNode("E: firmapost@barkarkitekter.no"));
-    copyright.appendChild(document.createTextNode("© 2020 Group 11"));
-    
-    node.appendChild(kontakt);
-    node.appendChild(gate);
-    node.appendChild(postnummer);
-    node.appendChild(telefon);
-    node.appendChild(epost);
-    node.appendChild(copyright);
-
-}  
-
-// triggers when ready 
-window.onload = () => buildNavbar(), buildFooter();
-
-
-/* When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
+/**
+* Checks if a given endpoint is the current one
+* @param {String} endpoint The endpoint to check
+* @returns {Boolean} true if it is current, else false
+*/
+function isCurrentEndpoint(endpoint) {
+  return endpoint === currentEndpoint;
 }
 
-<script>
-document.addEventListener(
-        "DOMContentLoaded", () => {
-            new Mmenu( "#my-menu", {
-                navbars: [{
-                    // first navbar options
-                }, {
-                    // second navbar options
-                }]
-            }, {
-                navbars: {
-                    // navbars configuration
-                }
-            });
-        }
-    );
-</script>*/
+/**
+* Creates an anchor element of a given page with link and content
+* @param {Object} page The page with text and endpoint properties
+* @returns {HTMLAnchorElement} The anchor element
+*/
+function createLinkToPage(page) {
+  let anchor = document.createElement("a");
+  if (page.text) {
+      anchor.appendChild(document.createTextNode(page.text));
+  } else {
+      let image = document.createElement("img");
+      image.src = page.img;
+      image.style.width = "100px";
 
-/*footer
+      anchor.appendChild(image);
+  }
+
+  if (isCurrentEndpoint(page.endpoint)) {
+      anchor.classList.add("navbar-current-item");
+  }
+
+  anchor.href = page.endpoint;
+
+  return anchor;
+}
+
+/**
+* Adds a page to the navigation bar as a link.
+* @param {Object} page The page to link
+* @param {HTMLDivElement} element The element to hold the link
+*/
+function addLinkToElement(page, element) {
+  const link = createLinkToPage(page);
+  element.appendChild(link);
+}
+
+/**
+* Returns the endpoint of the current page
+* @returns {String} The current endpoint
+*/
+function getCurrentEndpoint() {
+  const url = window.location.href;
+
+  return url.split("/").pop();
+}
+
+/**
+* Builds the navigation bar by adding html elements to it.
+*/
+function buildNavbar() {
+  currentEndpoint = getCurrentEndpoint();
+
+  const navbar = document.getElementById("navbar");
+  const content = document.createElement("div");
+  content.classList.add("content");
+  navbar.appendChild(content);
+
+  const items = document.createElement("div");
+  items.classList.add("navbar-items");
+  content.appendChild(items);
+
+  pages.forEach((page) => addLinkToElement(page, items));
+}
+
+
+
+
+
+
+//footer
 
 
 //const footer = document.getElementById("footer");
@@ -209,47 +122,84 @@ document.addEventListener(
 /**email
 
 *function addLinkToElement(email, element) {
- *   const link = createLinkToPage(email);
+*   const link = createLinkToPage(email);
 *    element.appendChild(link);
-  }*/
+}*/
+
+function buildFooter() {
+  /**var x = document.createElement("theFooter");
+   x.setAttribute("id", "myFooter");
+   document.body.appendChild(x);
+
+   var y = document.createElement("P");
+   var kontakt = document.createTextNode("Kontakt");
+   y.appendChild(kontakt);
+
+   document.getElementById("footer").appendChild("y");*/
+  var myNode = document.getElementById("sf");
+
+  console.log(myNode.id);
+  var kontakt = document.createElement("h4");
+  var adresse1 = document.createElement("p");
+  var adresse2 = document.createElement("p");
+  var copyright = document.createElement("p");
+
+  kontakt.classList.add("tit");
+  copyright.classList.add("footer");
+  adresse1.classList.add("adresse1");
+  adresse2.classList.add("adresse2");
+
+
+  kontakt.appendChild(document.createTextNode("Kontakt oss"));
+  adresse1.appendChild(document.createTextNode("Avaldsnesgata 95 c \n 4014 Stavanger"));
+  adresse2.appendChild(document.createTextNode("4014 StavangerT:  51 56 78 30 \n E: firmapost@barkarkitekter.no"));
+  copyright.appendChild(document.createTextNode("© 2020 Group 11"));
+
+  myNode.appendChild(kontakt);
+  myNode.appendChild(adresse1);
+  myNode.appendChild(adresse2);
+  myNode.appendChild(copyright);
 
 
 
-   /*__________________________________________________________
-    const footer = document.getElementById("myFooter");
+}
 
-    console.log(footer);
-
-    const text1 = document.createTextNode("Kontakt oss \t weutwegr");
-    footer.appendChild(text1);
-
-    const text2 = document.createTextNode("avajhfhj");
-    footer.appendChild(text2);
+// Event trigger when the DOM is ready (newer browsers)
+window.onload = () => buildNavbar(), buildFooter();
 
 
-    var kontakt = "Kontakt \n Avaldsnesgata 95 c \n 4014 Stavanger \n T:  51 56 78 30 \n E: firmapost@barkarkitekter.no";
-    document.getElementById("myFooter") = kontakt;*/
+/* When the user scrolls the page, execute myFunction
+ window.onscroll = function() {myFunction()};
 
-    /*var x = "John";        // x is a string
-    var y = new String("John");  // y is an object
+ // Get the navbar
+ var navbar = document.getElementById("navbar");
 
-    document.getElementById("myFooter") =
-    typeof x + "<br>" + typeof y;
+ // Get the offset position of the navbar
+ var sticky = navbar.offsetTop;
 
-    document.footer.appendChild(x,y);*/
+ // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+ function myFunction() {
+ if (window.pageYOffset >= sticky) {
+ navbar.classList.add("sticky")
+ } else {
+ navbar.classList.remove("sticky");
+ }
+ }
 
-
-
-
-
-    /**var x = document.createElement("theFooter");
-    x.setAttribute("id", "myFooter");
-    document.body.appendChild(x);
-
-    var y = document.createElement("P");
-    var kontakt = document.createTextNode("Kontakt");
-    y.appendChild(kontakt);
-
-    document.getElementById("footer").appendChild("y");
-    
-    _________________________________________________________*/
+ <script>
+ document.addEventListener(
+ "DOMContentLoaded", () => {
+ new Mmenu( "#my-menu", {
+ navbars: [{
+ // first navbar options
+ }, {
+ // second navbar options
+ }]
+ }, {
+ navbars: {
+ // navbars configuration
+ }
+ });
+ }
+ );
+ </script>*/
