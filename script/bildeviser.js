af84d2f46ef4ece416c8618121a438fe87e344af
iverbb <iverbb@stud.ntnu.no>
2020-10-25 12:47:38 +0800

    Added functionality to bildeviser for showing overlay images

--- script/bildeviser.js
+++ script/bildeviser.js
@@ -1,13 +1,31 @@
 // Nora
 
-function on() {
-    document.getElementById("overlay").style.display = "block";
+function on(img) {
+  let overlay = document.getElementById("overlay");
+  overlay.style.display = "block";
+  overlay.querySelector("img").src = img;
 }
   
 function off() {
     document.getElementById("overlay").style.display = "none";
 }
 
+function overlayImage() {
+  let projectImage = document.createElement("img");
+  projectImage.style.maxWidth = "75%";
+  projectImage.style.maxHeight = "75%";
+  projectImage.style.margin = "auto";
+  projectImage.style.display = "block";
+  projectImage.id = "overlayImage";
+
+  let overlay = document.createElement("div");
+  overlay.onclick = (() => off());
+  overlay.id = "overlay";
+  overlay.appendChild(projectImage);
+  document.body.prepend(overlay);
+}
+
+overlayImage();
 
 /* 
 
