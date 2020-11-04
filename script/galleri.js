07f58244d08f14b674c98c067a3ceac60f0d089e
iverbb <iverbb@stud.ntnu.no>
2020-11-04 14:23:59 +0800

    refactor galleri.js

--- script/galleri.js
+++ script/galleri.js
@@ -1,37 +1,42 @@
 // Hedvig
 
 function imageGrid(filerList) {
-    let imageGrid = document.getElementsByClassName("box-grid");
-    for (let i in filerList) {
-        let file = filerList[i];
-        let boxSingle = document.createElement("div");
-        boxSingle.setAttribute("class", "box-single");
-        imageGrid[0].appendChild(boxSingle);
-        let boxImage = document.createElement("img");
-        let imageSrc = "../img/" + file.fileName;
-        boxImage.setAttribute("class", "box-img");
-        boxImage.setAttribute("alt", "bilde");
-        boxImage.setAttribute("src", imageSrc);
-        boxSingle.appendChild(boxImage);
-        boxImage.onclick = (() => on(imageSrc));
-        let boxCaption = document.createElement("div");
-        boxCaption.setAttribute("class", "box-caption");
-        boxSingle.appendChild(boxCaption);
-        let boxCaptionP = document.createElement("p");
-        boxCaptionP.setAttribute("id", "box-caption");
-        boxCaptionP.innerHTML = file.projectName + " " + file.finishYear;
-        boxCaption.appendChild(boxCaptionP);
-    }
+  let imageGrid = document.getElementsByClassName("box-grid");
+  for (let i in filerList) {
+    let file = filerList[i];
+
+    let boxSingle = document.createElement("div");
+    //boxSingle.setAttribute("class", "box-single");
+    boxSingle.className = "box-single";
+    imageGrid[0].appendChild(boxSingle);
+
+    let imageSrc = "../img/" + file.fileName;
+    let boxImage = document.createElement("img");
+    boxImage.className = "box-img";
+    boxImage.alt = "bilde";
+    boxImage.src = imageSrc;
+    boxImage.onclick = (() => on(imageSrc));
+    boxSingle.appendChild(boxImage);
+
+    let boxCaption = document.createElement("div");
+    boxCaption.className = "box-caption";
+    boxSingle.appendChild(boxCaption);
+
+    let boxCaptionP = document.createElement("p");
+    boxCaptionP.id = "box-caption";
+    boxCaptionP.innerHTML = file.projectName + " " + file.finishYear;
+    boxCaption.appendChild(boxCaptionP);
+  }
 }
 
 function filterKronologiskByYear(arr) {
-    let sortedYear = arr.sort((a, b) => {
-      if (a.finishYear < b.finishYear) {return 1;}
-      if (b.finishYear < a.finishYear) {return -1;}
-      return 0;
-    });
-    console.log("Sorted Year: ", sortedYear);
-    return sortedYear;
+  let sortedYear = arr.sort((a, b) => {
+    if (a.finishYear < b.finishYear) {return 1;}
+    if (b.finishYear < a.finishYear) {return -1;}
+    return 0;
+  });
+  console.log("Sorted Year: ", sortedYear);
+  return sortedYear;
 }
 
 function filterByType(prosjektType) {
@@ -49,7 +54,7 @@ function onloadShow(type) {
       || type === "beliggenhet"
       || type  === "kronologisk") {
       type = "";
-  }
+    }
   imageGrid(filterByType(type));
 }
 
@@ -69,29 +74,29 @@ function onloadShowFritidsbolig(tags) {
 }
 
 function onloadShowIdeer() {
-    buildSortprojects(tags);
-    imageGrid(filterByType("ideer"));
+  buildSortprojects(tags);
+  imageGrid(filterByType("ideer"));
 }
 
 function onloadShowMindrebygg() {
-    buildSortprojects();
-    imageGrid(filterByType("mindre_bygg"));
+  buildSortprojects();
+  imageGrid(filterByType("mindre_bygg"));
 }
 
 function onloadShowNaering() {
-    buildSortprojects();
-    imageGrid(filterByType("naering"));
+  buildSortprojects();
+  imageGrid(filterByType("naering"));
 }
 
 function onloadShowKronologisk() {
-    buildSortprojects();
-    imageGrid(filterKronologiskByYear(imageLibrary.files));
+  buildSortprojects();
+  imageGrid(filterKronologiskByYear(imageLibrary.files));
 }
 
 function onloadProsjekttype() {
-    buildSortprojects(buttons);
+  buildSortprojects(buttons);
 }
 
 function onloadShowBeliggenhet() {
-    buildSortprojects();
+  buildSortprojects();
 }
