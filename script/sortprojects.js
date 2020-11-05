8de14a53cda6ee1af6c75f3ccad4742bde37ec45
Hedvig Kvassheim <hedvig.kvassheim@gmail.com>
2020-11-05 21:36:28 +0100

    formatted content

--- script/sortprojects.js
+++ script/sortprojects.js
@@ -1,79 +1,80 @@
 function buildHambar(destination, tags, name, mobile) {
 
-  // create div dropdown
-  let dropdown = document.createElement("div");
-  dropdown.className = ((destination === "navbarMobile") ? "mobile-" : "") + "dropdown";
-  dropdown.style = "float:right";
+    // create div dropdown
+    let dropdown = document.createElement("div");
+    dropdown.className = ((destination === "navbarMobile") ? "mobile-" : "") + "dropdown";
+    dropdown.style = "float:right";
 
-  console.log("tags: ", tags);
-  // create button
-  let button = document.createElement("button");
-  decorateButton(button, name);
-  button.id = destination + "button";
-  button.onclick = ((e) => toggleMenu(e, destination));
-  button.className = "dropbtn";
+    console.log("tags: ", tags);
+    // create button
+    let button = document.createElement("button");
+    decorateButton(button, name);
+    button.id = destination + "button";
+    button.onclick = ((e) => toggleMenu(e, destination));
+    button.className = "dropbtn";
 
-  // create div2 dropdown content
-  let myDropdown = document.createElement("div");
-  myDropdown.className = ((destination === "navbarMobile") ? "mobile-" : "") + "dropdown-content";
-  myDropdown.id = destination + "_dropdown";
+    // create div2 dropdown content
+    let myDropdown = document.createElement("div");
+    myDropdown.className = ((destination === "navbarMobile") ? "mobile-" : "") + "dropdown-content";
+    myDropdown.id = destination + "_dropdown";
 
-  tags.forEach
-  ( (button) =>
-    {let a = document.createElement("a");
-     a.innerText = button.name;
-     a.href      = button.link;
-     myDropdown.appendChild(a);
+    tags.forEach
+    ((button) => {
+        let a = document.createElement("a");
+        a.innerText = button.name;
+        a.href = button.link;
+        myDropdown.appendChild(a);
     });
 
-  // // append div2 and button to dropdown
-  dropdown.appendChild(button);
-  dropdown.appendChild(myDropdown);
+    // // append div2 and button to dropdown
+    dropdown.appendChild(button);
+    dropdown.appendChild(myDropdown);
 
-  // append everything to html
-  let target = document.getElementById(destination);
-  target.appendChild(dropdown);
+    // append everything to html
+    let target = document.getElementById(destination);
+    target.appendChild(dropdown);
 }
 
 function decorateButton(button, input) {
-  // Trivial regex to see if an image or a name is to be given
-  let isImg = input.match("\.svg|\.jpg|\.jpeg|\.png");
-  console.log("is it valifd?? ", isImg);
-  if (!isImg) {button.innerText = input;}
-  else {
-    button.style.backgroundImage = "url('" + input + "')";
-    button.style.width = "30px";
-    button.style.height = "30px";
-    button.style.backgroundSize = "contain";
-    button.style.backgroundRepeat = "no-repeat";
-  }
+    // Trivial regex to see if an image or a name is to be given
+    let isImg = input.match("\.svg|\.jpg|\.jpeg|\.png");
+    console.log("is it valifd?? ", isImg);
+    if (!isImg) {
+        button.innerText = input;
+    } else {
+        button.style.backgroundImage = "url('" + input + "')";
+        button.style.width = "30px";
+        button.style.height = "30px";
+        button.style.backgroundSize = "contain";
+        button.style.backgroundRepeat = "no-repeat";
+    }
 }
 
 function onloadProjectpages(tags) {
-  //buildNavbar();
-  buildSortprojects(tags);
-  //buildFooter();
+    //buildNavbar();
+    buildSortprojects(tags);
+    //buildFooter();
 }
 
 //onloadProjectpages();
 
 
-
-
 // When the user clicks on the button, toggle between hiding and showing the dropdown content
 function toggleMenu(e, destination) {
-  let event = e ? e : window.event;
-  event.stopPropagation();
+    let event = e ? e : window.event;
+    event.stopPropagation();
 
-  let displayStatus = document.getElementById(destination + "_dropdown").style.display;
-  if (displayStatus != "block") { displayStatus = "block"; }
-  else { displayStatus = "none"; }
-  document.getElementById(destination + "_dropdown").style.display = displayStatus;
+    let displayStatus = document.getElementById(destination + "_dropdown").style.display;
+    if (displayStatus != "block") {
+        displayStatus = "block";
+    } else {
+        displayStatus = "none";
+    }
+    document.getElementById(destination + "_dropdown").style.display = displayStatus;
 
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = ((e) =>
-    {
-      document.getElementById(destination + "_dropdown").style.display = "none";
+    // Close the dropdown menu if the user clicks outside of it
+    window.onclick = ((e) => {
+        document.getElementById(destination + "_dropdown").style.display = "none";
     });
 }
 
--- script/velgmeny.js
+++ script/velgmeny.js
@@ -1,23 +1,23 @@
 function loadjs(file, name) {
-  let script = document.createElement("script");
-  script.id = name;
-  script.src = file;
-  script.type = "text/javascript";
-  document.body.appendChild(script);
+    let script = document.createElement("script");
+    script.id = name;
+    script.src = file;
+    script.type = "text/javascript";
+    document.body.appendChild(script);
 }
 
 function velgmeny() {
-  let viewwidth
-      =  window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    let viewwidth
+        = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
-  console.log("view width: " + viewwidth);
-  if (viewwidth < 900) {
-    pc.style.display = "none";
-    mobile.style.display = "inline-block";
-  } else {
-    pc.style.display = "inline-block";
-    mobile.style.display = "none";
-  }
+    console.log("view width: " + viewwidth);
+    if (viewwidth < 900) {
+        pc.style.display = "none";
+        mobile.style.display = "inline-block";
+    } else {
+        pc.style.display = "inline-block";
+        mobile.style.display = "none";
+    }
 }
 
 loadjs("../script/mobilmeny.js", "mobile");
