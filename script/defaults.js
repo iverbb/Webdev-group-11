d816abbaf29eb9ac06a365ed2ce4ae88062a3308
iverbb <iverbb@stud.ntnu.no>
2020-11-05 23:18:32 +0800

    .

--- script/defaults.js
+++ script/defaults.js
@@ -5,15 +5,16 @@ function loadjsFiles(files) {
     script.src = file;
     script.type = "text/javascript";
     frag.appendChild(script);
+    console.log(file + " loaded");
   });
   document.body.appendChild(frag);
+  console.log(imageLibrary.projectName);
 }
 
 let files = [
   "../script/prosjektlist.js",
   "../script/sortprojects.js",
   "../script/galleri.js",
-  "../script/bildeviser.js",
   "../script/bildeviser.js",
   "../script/favicon.js",
   "../script/velgmeny.js"
